9c544c538ce686954ecb9d71610b67f793d85fd8
Murat Tokak <tokakmurat01@gmail.com>
2025-05-11 14:22:29 +0300

    EntityState Komutları, Generic Repository Sınıfı ve Ef Sınıfları oluşturma

---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        AppDbContext context = new AppDbContext(); 
+        private readonly DbSet<T> _object;         
+
+        public GenericRepository()
+        {
+            _object = context.Set<T>(); 
+        }
+
+        public void Delete(int id)
+        {
+            var entity = _object.Find(id);
+            if (entity != null)
+            {
+                context.Entry(entity).State = EntityState.Deleted;
+                context.SaveChanges(); 
+            }
+        }
+
+        public List<T> GetAll()
+        {
+            return _object.ToList(); 
+        }
+
+        public T GetById(int id)
+        {
+            return _object.Find(id); 
+        }
+
+        public void Insert(T entity)
+        {
+            context.Entry(entity).State = EntityState.Added;
+            context.SaveChanges(); 
+        }
+
+        public void Update(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges(); 
+        }
+
+    }
+}
